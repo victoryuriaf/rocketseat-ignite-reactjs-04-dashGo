a3826057da03ace6b2b37c525d984f41035edf1f
Victor Yuri <victoryuri.af@gmail.com>
2022-03-04 16:11:49 -0300

    implementing SignIn page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,50 @@
-export default function Home() {
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
   return (
-    <h1>dashGo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack
+          spacing="4"
+        >
+
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+          />
+
+          <Input
+            name="Password"
+            type="Password"
+            label="Password"
+          />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Login
+        </Button>
+
+      </Flex>
+    </Flex >
   )
 }
