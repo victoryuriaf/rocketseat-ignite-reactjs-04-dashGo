77e58a9adfe7a0b26584cf8a064ca770e9c0a763
Victor Yuri <victoryuri.af@gmail.com>
2022-06-20 17:48:05 -0300

    finishing the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import { Input } from '../components/Form/Input';
 
 type SignInFormData = {
@@ -7,8 +10,19 @@ type SignInFormData = {
   password: string;
 };
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail invalido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  })
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
@@ -41,6 +55,7 @@ export default function SignIn() {
             name="email"
             type="email"
             label="E-mail"
+            error={errors.email}
             {...register('email')}
           />
 
@@ -48,6 +63,7 @@ export default function SignIn() {
             name="Password"
             type="Password"
             label="Password"
+            error={errors.password}
             {...register('password')}
           />
         </Stack>
@@ -57,7 +73,7 @@ export default function SignIn() {
           mt="6"
           colorScheme="pink"
           size="lg"
-          isLoading={formState.isSubmitting}
+          isLoading={isSubmitting}
         >
           Login
         </Button>
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,10 +1,42 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import Link from "next/link";
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
+type CreateUserFormData = {
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+};
+
+const createUserFormSchema = yup.object().shape({
+    name: yup.string().required('Nome obrigatório'),
+    email: yup.string().required('E-mail obrigatório').email('E-mail invalido'),
+    password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
+    password_confirmation: yup.string().oneOf([
+        null, yup.ref('password')
+    ], 'As senhas precisam ser iguais')
+});
+
 export default function CreateUser() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm({
+        resolver: yupResolver(createUserFormSchema)
+    })
+
+    const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+    }
+
     return (
         <Box>
             <Header />
@@ -18,11 +50,12 @@ export default function CreateUser() {
             >
                 <Sidebar />
 
-                <Box
+                <Box as="form"
                     flex="1"
                     borderRadius={8}
                     bg="gray.800"
                     p={["6", "8"]}
+                    onSubmit={handleSubmit(handleCreateUser)}
                 >
                     <Heading
                         size="lg"
@@ -41,8 +74,19 @@ export default function CreateUser() {
                             spacing={["6", "8"]}
                             w="100%"
                         >
-                            <Input name="name" label="Full name" />
-                            <Input name="email" type="email" label="E-mail" />
+                            <Input
+                                name="name"
+                                label="Full name"
+                                error={errors.name}
+                                {...register('name')}
+                            />
+                            <Input
+                                name="email"
+                                type="email"
+                                label="E-mail"
+                                error={errors.email}
+                                {...register('email')}
+                            />
                         </SimpleGrid>
 
                         <SimpleGrid
@@ -50,8 +94,20 @@ export default function CreateUser() {
                             spacing={["6", "8"]}
                             w="100%"
                         >
-                            <Input name="password" type="password" label="Password" />
-                            <Input name="password_confirmation" type="password" label="Password confirmation" />
+                            <Input
+                                name="password"
+                                type="password"
+                                label="Password"
+                                error={errors.password}
+                                {...register('password')}
+                            />
+                            <Input
+                                name="password_confirmation"
+                                type="password"
+                                label="Password confirmation"
+                                error={errors.password_confirmation}
+                                {...register('password_confirmation')}
+                            />
                         </SimpleGrid>
                     </VStack>
 
@@ -71,7 +127,9 @@ export default function CreateUser() {
                                 </Button>
                             </Link>
                             <Button
+                                type="submit"
                                 colorScheme="pink"
+                                isLoading={isSubmitting}
                             >
                                 Save
                             </Button>
