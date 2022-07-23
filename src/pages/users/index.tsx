d0f122c52563d4b53bbc17771c04283a9cfeb710
Victor Yuri <victoryuri.af@gmail.com>
2022-07-23 15:31:14 -0300

    react query concepts

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,37 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from '@tanstack/react-query';
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const { data, isLoading, error } = useQuery(['users'], async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric"
+                })
+            }
+        })
+
+        return users;
+    }, {
+        staleTime: 1000 * 5, // 5 seconds
+    })
+
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -55,140 +81,83 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
-                    <Table
-                        colorScheme="whiteAlpha"
-                    >
-                        <Thead>
-                            <Tr>
-                                <Th
-                                    px={["4", "4", "6"]}
-                                    color="gray.300"
-                                    width="8"
-                                >
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>
-                                    User
-                                </Th>
-                                {isWideVersion && <Th>
-                                    SignIn Date
-                                </Th>}
-                                <Th width="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td
-                                    px={["4", "4", "6"]}
-                                >
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text
-                                            fontWeight="bold"
-                                        >
-                                            Victor Yuri
-                                        </Text>
-                                        <Text
-                                            fontWeight="sm"
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários.</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table
+                                colorScheme="whiteAlpha"
+                            >
+                                <Thead>
+                                    <Tr>
+                                        <Th
+                                            px={["4", "4", "6"]}
                                             color="gray.300"
+                                            width="8"
                                         >
-                                            victoryuri.af@gmail.com
-                                        </Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    17 de Março, 2022
-                                </Td>}
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Edit
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td
-                                    px={["4", "4", "6"]}
-                                >
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text
-                                            fontWeight="bold"
-                                        >
-                                            Victor Yuri
-                                        </Text>
-                                        <Text
-                                            fontWeight="sm"
-                                            color="gray.300"
-                                        >
-                                            victoryuri.af@gmail.com
-                                        </Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    17 de Março, 2022
-                                </Td>}
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Edit
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td
-                                    px={["4", "4", "6"]}
-                                >
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text
-                                            fontWeight="bold"
-                                        >
-                                            Victor Yuri
-                                        </Text>
-                                        <Text
-                                            fontWeight="sm"
-                                            color="gray.300"
-                                        >
-                                            victoryuri.af@gmail.com
-                                        </Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    17 de Março, 2022
-                                </Td>}
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Edit
-                                    </Button>
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-
-                    <Pagination />
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>
+                                            User
+                                        </Th>
+                                        {isWideVersion && <Th>
+                                            SignIn Date
+                                        </Th>}
+                                        <Th width="8"></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td
+                                                    px={["4", "4", "6"]}
+                                                >
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text
+                                                            fontWeight="bold"
+                                                        >
+                                                            {user.name}
+                                                        </Text>
+                                                        <Text
+                                                            fontWeight="sm"
+                                                            color="gray.300"
+                                                        >
+                                                            {user.email}
+                                                        </Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>
+                                                    {user.createdAt}
+                                                </Td>}
+                                                <Td>
+                                                    <Button
+                                                        as="a"
+                                                        size="sm"
+                                                        fontSize="sm"
+                                                        colorScheme="purple"
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                                    >
+                                                        Edit
+                                                    </Button>
+                                                </Td>
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
