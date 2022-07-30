1fa5e405c401f2c2aeec0e2b3a6a17976348d6b7
Victor Yuri <victoryuri.af@gmail.com>
2022-07-30 17:29:56 -0300

    pagination component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,14 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { theme } from '../styles/theme';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryClient';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Link, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -7,14 +8,26 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { api } from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error } = useUsers();
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(page);
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
+
+    async function handlePrefetchUser(userId: number) {
+        await queryClient.prefetchQuery(['user', userId], async () => {
+            const response = await api.get(`users/${userId}`)
+
+            return response.data;
+        }, {
+            staleTime: 1000 * 60 * 10,  // 10 minutes
+        })
+    }
 
     return (
         <Box>
@@ -46,10 +59,10 @@ export default function UserList() {
                         >
                             Users
 
-                            { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
                         </Heading>
 
-                        <Link href="/users/create" passHref>
+                        <NextLink href="/users/create" passHref>
                             <Button
                                 as="a"
                                 size="sm"
@@ -59,7 +72,7 @@ export default function UserList() {
                             >
                                 Create New
                             </Button>
-                        </Link>
+                        </NextLink>
                     </Flex>
 
                     {isLoading ? (
@@ -94,7 +107,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td
@@ -104,17 +117,19 @@ export default function UserList() {
                                                 </Td>
                                                 <Td>
                                                     <Box>
-                                                        <Text
-                                                            fontWeight="bold"
-                                                        >
-                                                            {user.name}
-                                                        </Text>
-                                                        <Text
-                                                            fontWeight="sm"
-                                                            color="gray.300"
-                                                        >
-                                                            {user.email}
-                                                        </Text>
+                                                        <Link>
+                                                            <Text color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}
+                                                                fontWeight="bold"
+                                                            >
+                                                                {user.name}
+                                                            </Text>
+                                                            <Text
+                                                                fontWeight="sm"
+                                                                color="gray.300"
+                                                            >
+                                                                {user.email}
+                                                            </Text>
+                                                        </Link>
                                                     </Box>
                                                 </Td>
                                                 {isWideVersion && <Td>
@@ -136,7 +151,11 @@ export default function UserList() {
                                     })}
                                 </Tbody>
                             </Table>
-                            <Pagination />
+                            <Pagination
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
                         </>
                     )}
                 </Box>
